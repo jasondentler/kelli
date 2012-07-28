bde7fb35cc0447f2dfd8d14873f9d13e4b85a082
Jason Dentler <jasondentler@gmail.com>
2012-07-28 17:28:18 -0500

    Set up a new session
    Set up a new session

---
+++ KelliPokerPlanning/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using KelliPokerPlanning.Models;
+using Microsoft.Web.Mvc;
+using MvcContrib.Filters;
+
+namespace KelliPokerPlanning.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet, ModelStateToTempData]
+        public ViewResult Index()
+        {
+            var values = new[] {"XS", "S", "M", "L", "XL", "2X"};
+            return View(new PokerSetup()
+                            {
+                                Session = Guid.NewGuid(),
+                                Values = string.Join(Environment.NewLine, values),
+                                IncludeQuestion = true,
+                                IncludeInfinity = true
+                            });
+        }
+
+        [HttpPost, ModelStateToTempData]
+        public RedirectToRouteResult Index(PokerSetup model)
+        {
+            if (!ModelState.IsValid)
+                return this.RedirectToAction(c => c.Index());
+            return this.RedirectToAction(c => c.Index());
+        }
+
+    }
+}
---
+++ KelliPokerPlanning/Models/PokerSetup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KelliPokerPlanning.Models
+{
+    public class PokerSetup
+    {
+        public string Values { get; set; }
+        public bool IncludeQuestion { get; set; }
+        public bool IncludeInfinity { get; set; }
+    }
+}

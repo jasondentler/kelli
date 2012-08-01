22e5ad714cfd3b1adbe582903212289b4f664a11
Jason Dentler <jasondentler@gmail.com>
2012-08-01 10:12:39 -0500

    Get user details
    Can't debug locally

--- KelliPokerPlanning/Scripts/Home/Index.js
+++ KelliPokerPlanning/Scripts/Home/Index.js
@@ -1,6 +1,21 @@
 ﻿/// <reference path="https://api.stackexchange.com/js/2.0/all.js" />
 /// <reference path="~/Scripts/lib/jquery.js"/>
 $(function () {
+
+    function getUserDetails(accessToken) {
+        var url = 'https://api.stackexchange.com/2.0/me'; //?order=desc&sort=reputation&site=stackoverflow
+        var params = {
+            access_token: accessToken,
+            filter: 'default',
+            key: pageData.key
+        };
+
+        $.getJSON(url, params, function (data) {
+            console.log('/me Success!');
+            console.log(data);
+        });
+    }
+
     SE.init({
         clientId: pageData.clientId,
         key: pageData.key,
@@ -15,16 +30,18 @@ $(function () {
     $('#login').click(function () {
         SE.authenticate({
             success: function (data) {
-                console.log('Success!');
-                console.log(data);
+                getUserDetails(data.accessToken);
             },
             error: function (data) {
-                console.log('Error!');
-                console.log(data);
+                $('#authError')
+                    .html('Sorry. Authentication failed. ' + data.errorName + ':' + data.errorMessage)
+                    .show();
             },
             scope: [],
-            networkUsers: true
+            networkUsers: false
         });
     });
+
+
 
 });
